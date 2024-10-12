9fbca60bf329c262a6c8c79440de1903db7a4361
Aravind <aravindkrishnan05520@gmail.com>
2024-10-12 18:41:14 +0530

    home-changes

---
+++ src/components/Stairs.jsx
@@ -0,0 +1,36 @@
+import { motion } from "framer-motion"
+
+// varients
+const stairAnimation = {
+    initial: {
+        top: "0%",
+    },
+    animate: {
+        top: "100%"
+    },
+    exit: {
+        top: ["100%", "0%"]
+    }
+}
+
+const totalSteps = 6;
+const reverseIndex = (index)=>{
+    return totalSteps - index - 1;
+}
+
+const Stairs = () => {
+  return (
+    <>
+      {[...Array(totalSteps)].map((_,index)=>{
+        return<motion.div key={index} variants={stairAnimation} initial='initial' animate='animate' exit='exit' transition={{
+            duration: 0.4,
+            ease: "easeInOut",
+            delay: reverseIndex(index) * 0.1,
+        }} className="h-full w-full bg-white relative"
+        />
+      })}
+    </>
+  )
+}
+
+export default Stairs
